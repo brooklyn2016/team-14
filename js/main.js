7a7ea1ab1b9b3e8ee331116d3b5015ee28489bd4
Eric Xie <Eric Xie>
2016-10-28 22:07:51 -0400

    front end wav file recording complete

---
+++ js/main.js
@@ -0,0 +1,44 @@
+    window.URL = window.URL || window.webkitURL;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    var stream;
+    var audio = document.querySelector('audio');
+    var onFail = function(e) {
+        console.log('Rejected!', e);
+    };
+
+    var onSuccess = function(s) {
+        var context = new webkitAudioContext();
+        var mediaStreamSource = context.createMediaStreamSource(s);
+        recorder = new Recorder(mediaStreamSource);
+        recorder.record();
+    }
+    var isRecording = false;
+
+    function startRecording() {
+        if (navigator.getUserMedia) {
+            navigator.getUserMedia({
+                audio: true
+            }, onSuccess, onFail);
+        } else {
+            console.log('navigator.getUserMedia not present');
+        }
+    }
+
+    function toggleRecording() {
+        isRecording = !isRecording;
+        if (isRecording) {
+            document.getElementById('recordButton').className = "fa fa-stop";
+            startRecording();
+        } else {
+            document.getElementById('recordButton').className = "fa fa-microphone";
+            stopRecording();
+        }
+
+    }
+
+    function stopRecording() {
+        recorder.stop();
+        recorder.exportWAV(function(s) {
+            audio.src = window.URL.createObjectURL(s);
+        });
+    }
