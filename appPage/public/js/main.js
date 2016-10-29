efde4dce4e413d13db6077b86f5da7707eaf92ca
Eric Xie <Eric Xie>
2016-10-28 22:49:37 -0400

    fixed deprecated AudioContext #MakeCodeGreatAgain

--- appPage/public/js/main.js
+++ appPage/public/js/main.js
@@ -7,7 +7,7 @@
     };
 
     var onSuccess = function(s) {
-        var context = new webkitAudioContext();
+        var context = new AudioContext();
         var mediaStreamSource = context.createMediaStreamSource(s);
         recorder = new Recorder(mediaStreamSource);
         recorder.record();
