1e34b580de06dc0d4ba2b9c13b3b34ee846377cc
timothyyao <timmyyao555@gmail.com>
2016-10-28 20:28:06 -0400

    basic json file reading

--- app/app.js
+++ app/app.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs'),
+    obj
+fs.readFile('data/input.json', convertSpeech)
+
+
+function convertSpeech(err, data) {
+    if (err) throw err
+    obj = JSON.parse(data)
+    transcript = obj.results[0].alternatives[0].transcript
+    console.log(transcript);
+    // You can now play with your datas
+}
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
